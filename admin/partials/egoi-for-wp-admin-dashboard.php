a960a5dbe8127f74160f75248b8fb28e2ddb93f1
wsantos <wsantos@e-goi.com>
2018-10-18 18:15:44 +0100

    add Subscribers statistics && add last five Subscribers table

--- admin/partials/egoi-for-wp-admin-dashboard.php
+++ admin/partials/egoi-for-wp-admin-dashboard.php
@@ -25,16 +25,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="container">
     <div class="columns">
         <div class="column">
-            <div class="toast dash-notifications">
-                <div class="dash-notifications__img">
-                    <figure class="avatar avatar-xl dash-notifications__icon-upgrade"></figure>
-                </div>
-                <div class="dash-notifications__text" >
-                    <h3 class="text-dark">Upgrade da Conta</h3>
-                    <p class="text-dark">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt</p>
-                </div>
-                <div class="dash-notifications__close">
-                    <button class="btn btn-clear"></button>
+            <div class="eg-dash-notifications notice is-dismissible">
+                <div class="toast dash-notifications">
+                    <div class="dash-notifications__img">
+                        <figure class="avatar avatar-xl dash-notifications__icon-upgrade"></figure>
+                    </div>
+                    <div class="dash-notifications__text" >
+                        <h3 class="text-dark">Upgrade da Conta</h3>
+                        <p class="text-dark">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -42,6 +41,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 </div>
 <!-- /Notifications -->
 
+
+<div class="container">
+    <div class="columns">
+        <div class="column">
+            <h1 class="dash-notifications--titles">Estatísticas dos subscritores</h1>
+        </div>
+    </div>
+</div>
 
 <!-- Subscriber Statistics -->
 <!-- Registrations made today -->
@@ -83,4 +90,64 @@ if ( ! defined( 'ABSPATH' ) ) {
         </div>
     </div>
   </div>
+</div>
+<!-- /Subscriber Statistics -->
+
+<div class="container">
+    <div class="columns subscriber-form mt-3">
+        <div class="column col-8">
+            <div class="subscriber-form__element">
+                <div class="subscriber-form__element__title">
+                    Últimos 5 subscritores
+                </div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Formulário</th>
+                            <th>Lista</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="active">
+                            <td>Maria Almeida</td>
+                            <td>mariaalmeida@e-goi.com</td>
+                            <td>Captação de Leads</td>
+                            <td>Marketing</td>
+                        </tr>
+                        <tr>
+                            <td>Maria Almeida</td>
+                            <td>mariaalmeida@e-goi.com</td>
+                            <td>Captação de Leads</td>
+                            <td>Marketing</td>
+                        </tr>
+                        <tr class="active">
+                            <td>Maria Almeida</td>
+                            <td>mariaalmeida@e-goi.com</td>
+                            <td>Captação de Leads</td>
+                            <td>Marketing</td>
+                        </tr>
+                        <tr>
+                            <td>Maria Almeida</td>
+                            <td>mariaalmeida@e-goi.com</td>
+                            <td>Captação de Leads</td>
+                            <td>Marketing</td>
+                        </tr>
+                        <tr class="active">
+                            <td>Maria Almeida</td>
+                            <td>mariaalmeida@e-goi.com</td>
+                            <td>Captação de Leads</td>
+                            <td>Marketing</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="column col-4">
+            <div class="subscriber-form__element">
+                <div class="">Subscritores por Formulário</div>
+            </div>
+        </div>
+    </div>
 </div>
