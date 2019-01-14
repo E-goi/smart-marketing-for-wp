f0ba5166866f539291723998c4e0960905ec27fc
procha <procha@e-goi.com>
2019-01-14 11:21:10 +0000

    Change Version to 2.7.3

--- egoi-for-wp.php
+++ egoi-for-wp.php
@@ -11,7 +11,7 @@ error_reporting(0);
  * Plugin Name:       Smart Marketing SMS and Newsletters Forms
  * Plugin URI:        https://www.e-goi.com/en/o/smart-marketing-wordpress/
  * Description:       Smart Marketing for WP adds E-goi's multichannel automation features to WordPress.
- * Version:           2.7.2
+ * Version:           2.7.3
  * Author:            E-goi
  * Author URI:        https://www.e-goi.com
  * License:           GPL-2.0+
@@ -25,7 +25,7 @@ if (!defined( 'WPINC' )) {
     exit;
 }
 
-define('SELF_VERSION', '2.7.2');
+define('SELF_VERSION', '2.7.3');
 
 if (!session_id()){
     session_start();
