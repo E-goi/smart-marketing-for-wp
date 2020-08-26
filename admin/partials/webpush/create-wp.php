44b3c8529a152a5a90924b5220619a4bab56bab0
Ricardo Santos <rsantos@e-goi.com>
2020-08-26 10:22:15 +0100

    fix: add missing files

---
+++ admin/partials/webpush/create-wp.php
@@ -0,0 +1,53 @@
+<section class="smsnf-content">
+    <div>
+        <form method="post" id="create_webpush_form" action="<?php admin_url('admin.php?page=egoi-4-wp-webpush&sub=create-wp') ?>">
+
+            <?php
+                wp_nonce_field('create_webpush_form', 'nonce');
+                settings_fields( Egoi_For_Wp_Admin::OPTION_NAME );
+                settings_errors();
+            ?>
+
+            <div class="smsnf-grid">
+                <div>
+                    <div class="smsnf-input-group">
+                        <label for="max_width"><?php _e('Web Push Title', 'egoi-for-wp'); ?></label>
+                        <p class="subtitle"><?php _e( 'When visitors subscribe, the URL will include...', 'egoi-for-wp' ); ?></p>
+                        <input style="max-width: 400px;" type="text" required
+                               name="create_wp_form[label]" autocomplete="off"
+                               placeholder="<?= __('Example: ' . get_bloginfo() ); ?>" />
+                    </div>
+
+                    <div class="smsnf-input-group">
+                        <label for="list"><?php _e( 'Web Push List', 'egoi-for-wp' ); ?></label>
+                        <p class="subtitle"><?php _e( 'Visitors opting in to your notifications will be kept in...' ,'egoi-for-wp' ); ?></p>
+                        <div class="smsnf-wrapper">
+
+                            <?php
+                            if(empty($lists)) {
+                                printf( __( 'No lists found, <a href="%s">are you connected to E-goi</a> and/or have created lists?', 'egoi-for-wp' ), admin_url( 'admin.php?page=egoi-for-wp' ) );
+                            }else{ ?>
+                                <select name="create_wp_form[list]" required class="form-select" ><?php
+                                    $array_list = '';
+                                    foreach($lists as $list) {
+
+                                        if($list->title){ ?>
+                                        <option value="<?php echo $list->listnum;?>" <?php selected($this->options_list['list'], $list->listnum); ?>>
+                                            <?php echo $list->title;?>
+                                            </option><?php
+                                            $array_list .= $list->listnum.' - ';
+                                        }
+                                    } ?>
+                                </select>
+                            <?php } ?>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="smsnf-input-group">
+                <input type="submit" value="<?php _e('Save Changes', 'egoi-for-wp');?>" />
+            </div>
+        </form>
+    </div>
+</section>
