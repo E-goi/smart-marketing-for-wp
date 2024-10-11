cb7628da62e5a1920e08c5e1f74e5b319dbae5c5
Luis Araujo <laraujo@e-goi.com>
2024-10-11 17:55:09 +0100

    fix(BB-25825): fix text in line 13

--- admin/partials/integrations/contact-form-7.php
+++ admin/partials/integrations/contact-form-7.php
@@ -10,7 +10,7 @@ $contact_forms = $this->egoiWpApi->getContactFormInfo();
 			<div class="container" style="display: flex;justify-content: center;align-items: center;flex-direction: column;min-height: 100%;">
 				<div>
 					<h2><?php _e( 'Plugin missing', 'egoi-for-wp' ); ?></h2>
-					<h4><?php _e( 'Make sure you have b gg 7 installed before trying to configure the integration.', 'egoi-for-wp' ); ?></h4>
+					<h4><?php _e( 'Make sure you have Contact Form 7 installed before trying to configure the integration.', 'egoi-for-wp' ); ?></h4>
 				</div>
 			<div>
 			<?php } else { ?>
