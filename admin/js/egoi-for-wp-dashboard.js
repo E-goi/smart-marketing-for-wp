70a6156e11f625fa60d7ad74ca46c3072cae577b
Luis Araujo <laraujo@e-goi.com>
2024-12-22 20:41:28 +0000

    improv(BB-26070): fix account loading

--- admin/js/egoi-for-wp-dashboard.js
+++ admin/js/egoi-for-wp-dashboard.js
@@ -11,14 +11,10 @@
 				},
 				function(response) {
 					var output = jQuery.parseJSON( response.data );
-					if (output.notifications.upgrade) {
-						$( '#notification_upgrade_account' ).removeClass( 'd-none' );
+					if (output){
+						$( '#account_content_loading' ).remove();
+						$( '.smsnf-dashboard-account__content__table' ).append( output.account );
 					}
-					if (output.notifications.limit) {
-						$( '#notification_account_limit' ).removeClass( 'd-none' );
-					}
-					$( '#account_content_loading' ).remove();
-					$( '.smsnf-dashboard-account__content__table' ).append( output.account );
 				}
 			);
 
