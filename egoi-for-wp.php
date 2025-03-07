b2269f8988fe7d9a726bd88c4ec3f82972eda0cf
Renato Gomes <rgomes.dev@gmail.com>
2025-03-07 13:32:00 +0000

    chore: bump version to 5.0.13

--- egoi-for-wp.php
+++ egoi-for-wp.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Smart Marketing SMS and Newsletters Forms
  * Plugin URI:        https://www.e-goi.com/en/o/smart-marketing-wordpress/
  * Description:       Smart Marketing for WP adds E-goi's multichannel automation features to WordPress.
- * Version:           5.0.12
+ * Version:           5.0.13
 
  * Author:            E-goi
  * Author URI:        https://www.e-goi.com
