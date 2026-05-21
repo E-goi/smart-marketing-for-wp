4fb7e797e5a1a566afc7f2217940133151136b38
Carlos Henriques <chenriques@e-goi.com>
2026-05-21 10:07:30 +0100

    hotfix(BB-28808): get data from other source

--- egoi-for-wp.php
+++ egoi-for-wp.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Smart Marketing SMS and Newsletters Forms
  * Plugin URI:        https://www.e-goi.com/en/o/smart-marketing-wordpress/
  * Description:       Smart Marketing for WP adds E-goi's multichannel automation features to WordPress.
- * Version:           5.1.13
+ * Version:           5.1.14
 
  * Author:            E-goi
  * Author URI:        https://www.e-goi.com
